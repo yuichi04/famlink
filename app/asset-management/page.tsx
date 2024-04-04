487c24e393216c3fbc55b9aede1f3392ae4a5215
yuichi04 <crgone.910428.en@gmail.com>
2024-04-05 01:13:03 +0900

    収入と支出の登録タブに入力欄を設置

--- app/asset-management/page.tsx
+++ app/asset-management/page.tsx
@@ -6,6 +6,10 @@ import SkeletonAssetsCard from "./(components)/skeleton-assets-card";
 import BudgetCard from "./(components)/budget-card";
 import BarChart from "@/components/bar-chart";
 import PieChart from "@/components/pie-chart";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 export default function Page() {
   return (
@@ -35,6 +39,31 @@ export default function Page() {
             </TabsList>
 
             <TabsContent
+              value="transaction"
+              className="grid grid-cols-1 gap-y-2 mt-0"
+            >
+              日付：
+              <DatePickerWithRange />
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="amount">金額</Label>
+                <Input id="amount" type="number" />
+              </div>
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="category">カテゴリ</Label>
+                <Input id="category" />
+              </div>
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="shop">お店</Label>
+                <Input id="shop" />
+              </div>
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="memo">メモ</Label>
+                <Textarea id="memo" />
+              </div>
+              <Button>登録</Button>
+            </TabsContent>
+
+            <TabsContent
               value="report"
               className="grid grid-cols-1 gap-y-2 mt-0"
             >
@@ -42,6 +71,14 @@ export default function Page() {
               <BudgetCard type="expenses" />
               <BarChart type="expenses" />
               <PieChart type="expenses" />
+            </TabsContent>
+
+            <TabsContent
+              value="settings"
+              className="grid grid-cols-1 gap-y-2 mt-0"
+            >
+              <Button>カテゴリ設定</Button>
+              <Button>支払い先設定</Button>
             </TabsContent>
           </Tabs>
         </TabsContent>
@@ -51,8 +88,33 @@ export default function Page() {
             <TabsList className="w-full grid grid-cols-3 bg-slate-300 mb-2">
               <TabsTrigger value="transaction">取引登録</TabsTrigger>
               <TabsTrigger value="report">レポート</TabsTrigger>
-              <TabsTrigger value="income">設定</TabsTrigger>
+              <TabsTrigger value="settings">設定</TabsTrigger>
             </TabsList>
+
+            <TabsContent
+              value="transaction"
+              className="grid grid-cols-1 gap-y-2 mt-0"
+            >
+              日付：
+              <DatePickerWithRange />
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="amount">金額</Label>
+                <Input id="amount" type="number" />
+              </div>
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="category">カテゴリ</Label>
+                <Input id="category" />
+              </div>
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="shop">お店</Label>
+                <Input id="shop" />
+              </div>
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="memo">メモ</Label>
+                <Textarea id="memo" />
+              </div>
+              <Button>登録</Button>
+            </TabsContent>
 
             <TabsContent
               value="report"
@@ -62,6 +124,14 @@ export default function Page() {
               <BudgetCard type="income" />
               <BarChart type="income" />
               <PieChart type="income" />
+            </TabsContent>
+
+            <TabsContent
+              value="settings"
+              className="grid grid-cols-1 gap-y-2 mt-0"
+            >
+              <Button>カテゴリ設定</Button>
+              <Button>支払い先設定</Button>
             </TabsContent>
           </Tabs>
         </TabsContent>
